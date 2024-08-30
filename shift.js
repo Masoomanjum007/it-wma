ee177e647ded30d48036b94a16b6203b3ec37dfc
Masoomanjum007 <masoomanjum007@gmail.com>
2024-08-30 09:50:17 +0500

    add all

---
+++ shift.js
@@ -0,0 +1,35 @@
+let fruits = ["Mangoes" , "orange" , "grapes"]
+fruits.shift()
+console.log(fruits)
+
+fruits.unshift("Apple")
+
+console.log(fruits)
+//for loooops...............//
+// for (let index = 1; index <= 10 ; index++)
+//     console.log(index + "x" + "2" + " =" + index * 2)
+// for (let index = 10; index >= 1 ; index--)
+//     console.log(index + "x" + "2" + " =" + index * 2)
+
+
+// let array =["one" , "two" ,"three" , "four" , "five"]
+// for (let index = 0; index < array.length; index++) {
+//     console.log(array[index])
+// }
+//forloooops..............//
+
+/////loops cont/////
+let firstNames = ["Masoom" , "Talha" , "Kabir" , "Ahmad", "Ali"]
+let lastNames = ["Anjum" , "Barakzai" , "Orakzai" , "Ali" , "khan"]
+let fullNames = [];
+
+//// looops cont/////
+
+/////Change case/////
+
+let firstName = "MaSoOm"
+console.log( firstName.toUpperCase())
+console.log(firstName.toLowerCase())
+
+/// chap 21///
+
