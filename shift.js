5ec8356b56e239d1910cc4fa51e49d14a6a0f088
Masoomanjum007 <masoomanjum007@gmail.com>
2024-09-26 21:22:54 +0500

    add

--- shift.js
+++ shift.js
@@ -1,35 +1,101 @@
-let fruits = ["Mangoes" , "orange" , "grapes"]
-fruits.shift()
-console.log(fruits)
+// let fruits = ["Mangoes" , "orange" , "grapes"]
+// fruits.shift()
+// console.log(fruits)
 
-fruits.unshift("Apple")
+// fruits.unshift("Apple")
 
-console.log(fruits)
-//for loooops...............//
-// for (let index = 1; index <= 10 ; index++)
-//     console.log(index + "x" + "2" + " =" + index * 2)
-// for (let index = 10; index >= 1 ; index--)
-//     console.log(index + "x" + "2" + " =" + index * 2)
+// console.log(fruits)
+// //for loooops...............//
+// // for (let index = 1; index <= 10 ; index++)
+// //     console.log(index + "x" + "2" + " =" + index * 2)
+// // for (let index = 10; index >= 1 ; index--)
+// //     console.log(index + "x" + "2" + " =" + index * 2)
 
 
-// let array =["one" , "two" ,"three" , "four" , "five"]
-// for (let index = 0; index < array.length; index++) {
-//     console.log(array[index])
-// }
-//forloooops..............//
+// // let array =["one" , "two" ,"three" , "four" , "five"]
+// // for (let index = 0; index < array.length; index++) {
+// //     console.log(array[index])
+// // }
+// //forloooops..............//
 
-/////loops cont/////
-let firstNames = ["Masoom" , "Talha" , "Kabir" , "Ahmad", "Ali"]
-let lastNames = ["Anjum" , "Barakzai" , "Orakzai" , "Ali" , "khan"]
-let fullNames = [];
+// /////loops cont/////
+// let firstNames = ["Masoom" , "Talha" , "Kabir" , "Ahmad", "Ali"]
+// let lastNames = ["Anjum" , "Barakzai" , "Orakzai" , "Ali" , "khan"]
+// let fullNames = [];
 
-//// looops cont/////
+// //// looops cont/////
 
-/////Change case/////
+// /////Change case/////
 
-let firstName = "MaSoOm"
-console.log( firstName.toUpperCase())
-console.log(firstName.toLowerCase())
+// let firstName = "MaSoOm"
+// console.log( firstName.toUpperCase())
+// console.log(firstName.toLowerCase())
 
-/// chap 21///
+// /// chap 21///
+// let fruit = ["apple" , "mango" , "banana" , "grape" , "lemon"]
+// fruit.splice(0,4,"3")
+// console.log(fruit)
+
+// //let Name= "masoom"
+// //console.log(Name)
+
+// let xyz = "waseem"
+// consol .log(xyz[0].toUpperCase() + xyz.slice(2))
+
+// let num = Math.random()
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(2.3))
+// console.log(Math.floor(2.9))
+
+// let num = Math.random()  // 0.1 -----0.99   * 10 or 6
+// console.log(num *6 + 1)
+////////////
+// let num = Math.random() 
+// console.log(num)
+// console.log(num * 10)
+
+// console.log(Math.round(0.89))
+// console.log (Math.ceil(0.4))
+// console.log(Math.floor(0.5))
+
+// let num = "2kg"
+// let newnum = parseFloat(num)
+// let num2 = "4L"
+// let newnum2 = parseFloat(num2)
+// console.log (num + num2)
+// console.log (newnum + newnum2)
+
+// let kg = "25"
+// console.log (Number(kg) + 3)
+// console.log (kg + 3)
+console.log(Math.random() * 10)
+let xyz = 5.6748439219383347623487379347624723897234234
+console.log(xyz.toFixed(2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
